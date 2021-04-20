e06bcd9cce35a72548cc56f3ceb4e17c1d05afee
ti4 Gimi <tonyadryanogomes1@gmail.com>
2021-04-20 15:59:35 -0300

    NLW #4 - Projeto aula 2

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -3,7 +3,7 @@ import styles from "../styles/components/Countdown.module.css";
 
 export function Countdown() {
   const [time, setTime] = useState(22.5 * 60);
-  const [active, setActive] = useState(false);
+  const [isActive, setIsActive] = useState(false);
 
   const minutes = Math.floor(time / 60);
   const seconds = time % 60;
@@ -12,18 +12,18 @@ export function Countdown() {
   const [secondLeft, secondRight] = String(seconds).padStart(2, "0").split("");
 
   function startCountdown() {
-    active ? setActive(false) : setActive(true);
-    // setActive(true);
+    isActive ? setIsActive(false) : setIsActive(true);
+    // setIsActive(true);
   }
 
   useEffect(() => {
-    if (active && time > 0) {
+    if (isActive && time > 0) {
       setTimeout(() => {
         setTime(time - 1);
       }, 1000);
     }
-  }, [active, time]);
-  //executar função / quando isso acontecer
+  }, [isActive, time]);
+  //()=> {executar função},  [quando isso mudar ]
 
   return (
     <div>
